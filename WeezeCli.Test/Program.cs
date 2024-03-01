4dbaaf6d6200764b8395eca6ae55407c6b5fd094
RainkLH <withrain@outlook.com>
2024-03-01 18:54:17 +0800

    调整仓库文件结构

---
+++ WeezeCli.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace WeezeCli.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeezeCliApp weezeCliHelper = new WeezeCliApp("");
+            weezeCliHelper.Register(new Test());
+            weezeCliHelper.Register(new Test2());
+            if (!weezeCliHelper.ParseAndInvoke(args, out string message))
+                Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+
+        [CommandGroup("测试", asMainApp: true)]
+        public class Test
+        {
+            [Command("Add方法")]
+            public void Add(string arg1, string arg2)
+            {
+                Console.WriteLine($"Test.Add {arg1}, {arg2}");
+            }
+        }
+
+        [CommandGroup("Test2")]
+        public class Test2
+        {
+            [Command("Add方法")]
+            public void Add(string arg1, string arg2)
+            {
+                Console.WriteLine("Test2.Add {arg1}, {arg2}");
+            }
+        }
+    }
+}
